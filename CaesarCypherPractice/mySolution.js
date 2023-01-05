0d46423ebadb7abf44d1af08fb9e5f23d70b7028
Justin <just2rules@gmail.com>
2023-01-04 17:50:22 -0800

    completed attempt, fixed issues in original thinking and methodology

--- CaesarCypherPractice/mySolution.js
+++ CaesarCypherPractice/mySolution.js
@@ -34,7 +34,10 @@ function readLine() {
 function caesarCipher(s, k) {
     // Write your code here
     //get alphabet array
+
     //use shift and push to rotate our array to the cipher (can use splice for this too)
+        //Edit: This was a bad idea especially if k becomes longer than the string, splice wouldn't work here
+
     //then make a hashtable with keys using the original alphabet, values would be the new alphabet
     //iterate through the input string, make a new output string, append to output string value at key when encounter letter
 
@@ -43,8 +46,13 @@ function caesarCipher(s, k) {
     let cypherArray = [...alphabetArray]
 
     //start cypher process
-    let removedElements = cypherArray.splice(0,k);
-    cypherArray = cypherArray.concat(removedElements)
+    // let removedElements = cypherArray.splice(0,k);
+    // cypherArray = cypherArray.concat(removedElements)
+
+    //Editted process
+    for(let i = 0; i < k; i++){
+        cypherArray.push(cypherArray.shift())
+    }
 
     //create hash table
     const cypherMap = {}
@@ -52,7 +60,7 @@ function caesarCipher(s, k) {
     for(let i = 0; i < alphabetArray.length; i++){
         cypherMap[alphabetArray[i]] = cypherArray[i];
     }
-
+    console.log(cypherMap)
     //start conversion of input string
     let answer =""
 
@@ -74,6 +82,9 @@ function caesarCipher(s, k) {
         }
     }
     return answer;
+
+    //What I learned, take time to think about my process a bit more and run it through with a small test case and think about
+    //how the solution might look with a larger test case and see if my algorithm still works
 }
 
 function main() {
